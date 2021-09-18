8e2ecc04fd1dad27ad7aa045ecdf26631c85ee59
Pramod Vaghasia <pramodv997@gmail.com>
2021-09-18 21:34:30 +0530

    Add files via upload

---
+++ Prototype_Design_Pattern/DesignPatterns/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prototype Design Pattern
+
+            Users users1 = new Users(1, "Johan", "patel", "joahan123@gmail.com", "johan111");
+            users1.createAccount();
+
+            Users users2 = (Users)users1.getClone();
+            users2.createAccount();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Prototype_Design_Pattern/DesignPatterns/Users.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    class Users : IPrototype
+    {
+        int userId;
+        string firstName;
+        string lastnameName;
+        string email;
+        string password;
+
+        public Users()
+        {
+
+        }
+
+        public Users(int userId, string firstName, string lastnameName, string email, string password)
+        {
+            this.userId = userId;
+            this.firstName = firstName;
+            this.lastnameName = lastnameName;
+            this.email = email;
+            this.password = password;
+        }
+
+        public IPrototype getClone()
+        {
+            return new Users(userId, firstName, lastnameName, email, password);
+        }
+
+        public void createAccount()
+        {
+            Console.WriteLine("{0} {1} {2} {3} {4}", userId, firstName, lastnameName, email, password);
+        }
+
+    }
+
+  
+}
