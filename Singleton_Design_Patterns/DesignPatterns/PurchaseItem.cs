9bd025925be16de0d3c93b57bdad31fe8cf351fc
Pramod Vaghasia <pramodv997@gmail.com>
2021-09-18 21:34:49 +0530

    Add files via upload

---
+++ Singleton_Design_Patterns/DesignPatterns/PurchaseItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    class PurchaseItem : Users
+    {
+        int itemId;
+        string itemName;
+        decimal price;
+        decimal totalAmount;
+        public static PurchaseItem purchaseItem;
+
+
+        //accessing singleton instance method
+        public static PurchaseItem Instance
+        {
+            get
+            {
+                if (purchaseItem == null)
+                {
+                    //singleton instance created only once
+                    purchaseItem = new PurchaseItem();
+                }
+                return purchaseItem;
+            }
+        }
+
+        public void buyItem()
+        {
+            Console.WriteLine("Item Purchased.");
+        }
+    }
+}
---
+++ Singleton_Design_Patterns/DesignPatterns/Users.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    class Users
+    {
+        int userId;
+        string firstName;
+        string lastnameName;
+        string email;
+        string password;
+
+        public void makePayment()
+        {
+            //getting singleton instance object
+            PurchaseItem purchaseItem = PurchaseItem.Instance;
+            purchaseItem.buyItem();
+
+        }
+
+    }
+
+}
